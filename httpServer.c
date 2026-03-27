3e136ed814b2facaa86d4bc09b358abc7c839cac
zainabad <zainabad27@gmail.com>
2026-03-27 18:41:05 +0500

    getaddrinfo func learning.

--- httpServer.c
+++ httpServer.c
@@ -1,61 +1,42 @@
+#define _POSIX_C_SOURCE 200112L
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include<unistd.h>
 
 int main()
 {
-    int Socketfd = socket(AF_INET, SOCK_STREAM, 0);
-    char buffer[128];
-    char peerIp[128];
-    struct sockaddr_in *my_Addr;
-    struct sockaddr_storage *peer_Addr;
-    
-    if (Socketfd < 0)
-    {
-        dprintf(2, "Socket did not get initialized at line number %i", (__LINE__ - 5));
-    }
-    else
-    {
-        printf("Socket Got initialized with File descriptor %i", Socketfd);
-    }
+    struct addrinfo hints;
 
-    my_Addr->sin_family = AF_INET;
-    my_Addr->sin_port = htons(2020);
-    // my_Addr->sin_zero=memset();
-    int statusOfIP = inet_pton(AF_INET, "192.168.1.253", &(my_Addr->sin_addr.s_addr));
-    if (statusOfIP <= 0)
+    struct addrinfo *result, *temp;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    int res = getaddrinfo(NULL, "8080", &hints, &result);
+    if (res != 0)
     {
-        if (statusOfIP == 0)
-        {
-            printf("Invalid string was a given as the IP address.");
-        }
-        else
-        {
-            fprintf(stderr, "invalid AddressFamily for the ip address.\nerror Type: %s",errno);
-        }
+        fprintf(stderr,"error while getting the address info from getaddrinfo: %s",gai_strerror((res)));
+        return 1;
     }
 
-
-    int Binded = bind(Socketfd, (struct sockaddr *)&my_Addr, sizeof(my_Addr));
-    if (Binded < 0)
+    for (temp = result; temp != NULL; temp = temp->ai_next)
     {
+        int sfd = socket(temp->ai_family, temp->ai_socktype, temp->ai_protocol);
 
-        dprintf(2, "Socket did not get binded at line number %i", (__LINE__ - 3));
+        if (sfd == -1)
+            continue;
+
+        if (bind(sfd, temp->ai_addr, temp->ai_addrlen) == 0)
+            break;
+
+        close(sfd);
     }
-
-    int startedlistening = listen(Socketfd, SOMAXCONN);
-
-    if (startedlistening < 0)
-    {
-        dprintf(2, "Socket did not started listening at line number %i", (__LINE__ - 4));
-    }
-    
-
-    int connectedSocketfd = accept(Socketfd, (struct sockaddr *)&peer_Addr, &sizeof(peer_Addr)); // the peer ip address is now at peeraddr pointer address.
-    // have to check if connected socket got initialized
-
-    inet_ntop(sizeof(peer_Addr),peer_Addr->ss_family,peer_Addr->,peerIp);
-
-    connect(connectedSocketfd, (struct sockaddr *)&peer_Addr, sizeof(peer_Addr));
+    freeaddrinfo(result);
+    freeaddrinfo(temp);
 }
