ddf73bca261025dd9fd36e94deeb98d896ce0bda
zainabad <zainabad27@gmail.com>
2026-03-28 16:41:50 +0500

    first manuall ECHO server in C written.

--- httpServer.c
+++ httpServer.c
@@ -6,37 +6,79 @@
 #include <sys/types.h>
 #include <netdb.h>
 #include <string.h>
-#include<unistd.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define const BUFF_SIZE = 64
 
 int main()
 {
-    struct addrinfo hints;
+    char MainBuffer[128];
 
-    struct addrinfo *result, *temp;
+    char ipstr[INET6_ADDRSTRLEN];
+    struct sockaddr_storage peerAddr;
+    int peerAddrLength = sizeof(peerAddr);
+
+    struct addrinfo hints, *res = NULL, *temp = NULL;
+    int MainSocketFd;
+
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
 
-    int res = getaddrinfo(NULL, "8080", &hints, &result);
-    if (res != 0)
+    int status = getaddrinfo(NULL, "8080", &hints, &res);
+
+    if (status != 0)
     {
-        fprintf(stderr,"error while getting the address info from getaddrinfo: %s",gai_strerror((res)));
+        fprintf(stderr, "Coudn't get the addresses to initialize the sockets. Error: %s", gai_strerror(status));
         return 1;
     }
 
-    for (temp = result; temp != NULL; temp = temp->ai_next)
+    for (temp = res; temp != NULL; temp = temp->ai_next)
     {
-        int sfd = socket(temp->ai_family, temp->ai_socktype, temp->ai_protocol);
+        MainSocketFd = socket(temp->ai_family, temp->ai_socktype, temp->ai_protocol);
 
-        if (sfd == -1)
+        if (MainSocketFd == -1)
             continue;
 
-        if (bind(sfd, temp->ai_addr, temp->ai_addrlen) == 0)
+        if (bind(MainSocketFd, temp->ai_addr, temp->ai_addrlen) == 0)
             break;
 
-        close(sfd);
+        close(MainSocketFd);
     }
-    freeaddrinfo(result);
-    freeaddrinfo(temp);
+
+    freeaddrinfo(res);
+
+    if (listen(MainSocketFd, 5) == -1)
+    {
+        
+        perror("Listening error");
+        close(MainSocketFd);
+        return 1;
+    }
+    fprintf(stdout, "Started Listening to the port 8080......\n");
+    
+    int CurrentLyConnectedFd;
+
+    CurrentLyConnectedFd = accept(MainSocketFd, (struct sockaddr *)&peerAddr, &peerAddrLength);
+
+    if (CurrentLyConnectedFd == -1)
+        perror("error occured while accepting the socket connection.");
+
+    int BytesReceived = recv(CurrentLyConnectedFd, MainBuffer, 128, 0);
+    if (BytesReceived == -1)
+        perror("error occured while receiving the data from connected socket.");
+    else if (BytesReceived == 0)
+    {
+        printf("Can't Revcieve Message Anymore Socket is shut down.");
+    }
+
+    int BytesSend = send(CurrentLyConnectedFd, MainBuffer, BytesReceived, 0);
+    if (BytesSend == -1)
+        perror("error occured while sending the data to the connected socket.");
+
+    write(2,"Received Data Sent back.\n",strlen("Received Data Sent back.\n "));
+    shutdown(CurrentLyConnectedFd, SHUT_RDWR);
+    shutdown(MainSocketFd, SHUT_RDWR);
+    return 0;
 }
